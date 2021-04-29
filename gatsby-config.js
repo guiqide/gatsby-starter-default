077b763679bccaf3369177a287c0b444d39757c7
kevingui <kevingui@tencent.com>
2021-04-29 16:29:20 +0800

    feat: 替换成最新的google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,9 +24,17 @@ module.exports = {
     },
     'gatsby-plugin-styled-components',
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: 'gatsby-plugin-google-gtag',
       options: {
-        trackingId: 'G-NTKEDE94VP',
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-0RE29VWFYM', // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          cookie_expires: 0,
+        },
       },
     },
     {
